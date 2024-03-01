d038cf38bdd869363984688f9aa0f5c0a05a306f
ThomasB013 <thomasbroekman2002@gmail.com>
2024-03-01 17:33:55 +0100

    checkpoint not found etc.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
 
+    experimental: {
+        serverActions: {
+          // edit: updated to new key. Was previously `allowedForwardedHosts`
+          allowedOrigins: ['127.0.0.1:3000'],
+        },
+    },    
+};
 module.exports = nextConfig;
+
